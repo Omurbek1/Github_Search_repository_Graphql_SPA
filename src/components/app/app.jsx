1316dcbd41d56a05df5050380dd8d5f43fd8d48a
unknown <omurbekmamytbekov545@gmail.com>
2022-01-19 12:59:19 +0600

    github search repository

---
+++ src/components/app/app.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import Main from "../main";
+import Card from "../card";
+import "./app.css";
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/" component={Main} exact />
+        <Route
+          path="/:id"
+          render={(routeProps) => {
+            return <Card props={routeProps.match.params.id} />;
+          }}
+        />
+      </Switch>
+    </HashRouter>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import App from "./components/app";
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    authorization: `Bearer ${process.env.GRAPHQL_API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
+
+ReactDom.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.querySelector("#root")
+);
